af0435ebae24b0a3eb8feeccebe2c6a052d943e9
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-12 00:03:58 -0500

    12 solved

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -12,10 +12,21 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 
 function isSorted(arr) {
-  // Your code here 
+  let last = arr[arr.length - 1]
+  let secondToLast = arr[arr.length - 2]
+  if (arr.length === 1) return true;
+
+  if (secondToLast > last) return false;
+  arr.pop()
+
+  return isSorted(arr)
 }
 
-
+// debugger
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = isSorted;
