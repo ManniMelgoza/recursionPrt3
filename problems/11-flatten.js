c6ec6fa2d8224718836cb84ecc72635800456fc7
Manni <emmanuelmelgoza1@gmail.com>
2024-04-11 22:36:53 -0600

    Solved 11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -12,10 +12,27 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
+// flatten` that takes a single array
 function flatten(arr) {
-  // Your code here 
-}
-  
+  //Create an empty array to store all values
+  let newArr = []
+
+  for (let i = 0; i < arr.length; i++){
+
+    if(Array.isArray(arr[i])){
+      newArr = newArr.concat(flatten(arr[i]));
+    }
+    else{
+      newArr.push(arr[i])
+    }
+  }
+return newArr;
+  // returns and array with all the nested
+};
+
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
